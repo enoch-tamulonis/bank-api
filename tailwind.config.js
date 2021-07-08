d42647301b52a8bf8cb5ed11c7b978eb6d1301df
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-07-07 20:38:58 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      minHeight: {
+        'screen': '100vh',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+    plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+
+}
